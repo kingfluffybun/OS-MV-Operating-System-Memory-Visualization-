2f5f212dd01d005ba0a8d99f431a43c81a6b15c2
harB33 <bautistaharvy@gmail.com>
2026-03-08 03:54:46 +0800

    Sidebar Toggle | Sidebar Submenu Toggle

---
+++ util/script.js
@@ -0,0 +1,25 @@
+const toggleButton = document.getElementById('toggle-btn');
+const sidebar = document.getElementById('sidebar');
+const logo = document.getElementById('logo');
+const logoH1 = document.getElementById('h1');
+
+const toggleSideBar = () => {
+    sidebar.classList.toggle('close');
+    toggleButton.classList.toggle('rotate');
+    logo.classList.toggle('hidden');
+    logoH1.classList.toggle('hidden');
+
+    Array.from(sidebar.getElementsByClassName('show')).forEach(element => {
+        element.classList.remove('show');
+        element.previousElementSibling.classList.remove('rotate');
+    });
+}
+
+const toggleSubMenu = button => {
+    if(sidebar.classList.contains('close')) {
+        toggleSideBar();
+    }
+    
+    button.nextElementSibling.classList.toggle('show');
+    button.classList.toggle('rotate');
+}
