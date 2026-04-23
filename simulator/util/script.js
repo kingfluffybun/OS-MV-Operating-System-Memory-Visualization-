48472342dc22e9fdbdeebd2e80171af8b61f3643
Fluffybun <Clarence.luna651@gmail.com>
2026-04-23 19:10:43 +0800

    change icon for login sa guest

--- simulator/util/script.js
+++ simulator/util/script.js
@@ -90,9 +90,11 @@ function loadCurrentUser() {
 
   if (stored && stored.username) {
     username.textContent = stored.username;
+    document.getElementById("in-out-icon").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-log-out-icon lucide-log-out"><path d="m16 17 5-5-5-5"/><path d="M21 12H9"/><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/></svg>`;
     document.getElementById("in-out").innerHTML = "Logout";
   } else {
     username.textContent = "Guest";
+    document.getElementById("in-out-icon").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-log-in-icon lucide-log-in"><path d="m10 17 5-5-5-5"/><path d="M15 12H3"/><path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"/></svg>`
     document.getElementById("in-out").innerHTML = "Login";
   }
 }
